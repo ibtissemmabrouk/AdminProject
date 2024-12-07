b6582c74d1f77282b4766b4cc32a5b165f58fec1
ibtissemmabrouk <ibtissem102030@gmail.com>
2024-12-07 19:01:46 +0000

    Update

--- SourceCode/MainIndex.js
+++ SourceCode/MainIndex.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
  
 app.get('/', (req, res) => {
-	  res.send('<h1> ISIMM 24/25 : ING2 – Semester 1</h1>');
+	  res.send('<h1> ISIMM 24/25 : ING2 – Semester 1 kkl</h1>');
 });
  
 module.exports = app;
