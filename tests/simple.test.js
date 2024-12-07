d44bceee0269473812453498f9fd057343c9913b
ibtissemmabrouk <ibtissem102030@gmail.com>
2024-12-07 19:24:34 +0000

    with buildPush actions 2

--- tests/simple.test.js
+++ tests/simple.test.js
@@ -4,6 +4,6 @@ const supertest = require("supertest");
 test("GET /", done => {
   supertest(app)
     .get("/")
-    .expect(200, "<h1> ISIMM 24/25 : ING2 </h1>" )
+    .expect(200, "<h1> ISIMM 24/25 : ING2  </h1>" )
     .end(done)
 })
